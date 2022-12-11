632a780fe7887c75c0c9036f5fb87f43248803a2
Jokjeck <119483588+MertJokjeck@users.noreply.github.com>
2022-12-11 13:21:31 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:petgoapp/screens/todolist.dart';
+
+import 'screens/counter.dart';
+import 'screens/nalan.dart';
+import 'screens/toplama.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '9219-Mert Özoğul',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const Todolist(),
+    );
+  }
+}
